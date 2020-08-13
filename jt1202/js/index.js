4c6f414215ce2d42d2b0396f9f50cc979d5db397
ndtich <ndtich@gmail.com>
2020-08-13 08:55:44 +0700

    copy to jt1202

---
+++ jt1202/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".introduction_article").animate({opacity: 1}, 2000);
+    }, 1000)
+
+    $(".navigation a").click(function() {
+        $(".navigation a").removeClass("current");
+        $(this).addClass("current");
+    })
+
+    $(document).on("scroll", onScroll);
+})
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    if (scrollPos < 400) {
+        $(".navigation a").removeClass("current");
+        $(".navigation a").first().addClass("current");
+    } else {
+        $(".navigation a").each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos + 1 && refElement.position().top + refElement.outerHeight() > scrollPos + 1) {
+                $(".navigation a").removeClass("current");
+                currLink.addClass("current");
+            }
+            else {
+                currLink.removeClass("current");
+            }
+        });
+    }
+}
